64db22baeb0dc1a9feb5133ea1361acfdaf4241f
Sonia Villa <ainosia@gmail.com>
2018-04-16 14:53:45 +0200

    Primera versión

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCqx0e3jxLzBqe7M_5PTDGq5L9Zg4HABl8",
+    authDomain: "firechat-f11e7.firebaseapp.com",
+    databaseURL: "https://firechat-f11e7.firebaseio.com",
+    projectId: "firechat-f11e7",
+    storageBucket: "firechat-f11e7.appspot.com",
+    messagingSenderId: "694468415082"
+  }
 };
